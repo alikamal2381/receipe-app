ee81a55aebd3631d141df98e830553c20d99f781
AliKamal <alikamal2381@gmail.com>
2020-05-22 23:54:22 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reace - Built Receipe App
         </a>
       </header>
     </div>
